1976ac24286a0c76f3a570942f47934e889d33ea
game2007-lsy <811416449@qq.com>
2026-09-20 23:08:10 +0800

    Update base64_text.py

--- base64_text.py
+++ base64_text.py
@@ -1,8 +1,6 @@
 import base64#引入base64工具箱
-
-text = "liushuoyu"
-encoded = base64.b64encode(text.encode('utf-8'))
-print("编码结果:",encoded.decode('utf-8'))
-
-decoded = base64.b64decode(encoded).decode('utf-8')
-print("解码结果:", decoded)
+text = "liushuoyu"#要编码的内容
+encoded = base64.b64encode(text.encode('utf-8'))#编码
+print("编码结果:",encoded.decode('utf-8'))#打印编码结果
+decoded = base64.b64decode(encoded).decode('utf-8')#解码
+print("解码结果:", decoded)#打印解码结果
